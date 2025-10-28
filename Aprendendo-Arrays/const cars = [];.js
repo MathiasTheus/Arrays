5d3250be7c54f1ee2e2602c5d1d14af6f9215b69
Mathias <matheussantosluiz82@gmail.com>
2025-10-28 11:48:34 -0300

    Add explicaçoes e inseri mais codigos

--- Aprendendo-Arrays/const cars = [];.js
+++ Aprendendo-Arrays/const cars = [];.js
@@ -1,3 +1,30 @@
+// matriz vazia
 const cars = [];
 
 console.log(cars);
+// add 3 elementos em uma matriz vazia
+cars[0] = "Peugeot208";
+cars[1] = "Volvo";
+cars[2] = "Fluence";
+console.log(cars);
+
+//length retorna o comprimento de uma matriz
+console.log(cars.length); // car possui 3 elementos
+
+
+//acessando o ultimo elemento da matriz
+console.log(cars[cars.length -1]);
+
+//variavel carros armazena o comprimento do array cars que eh 3
+let carros = cars.length;
+for(let i = 0; i < carros; i++){
+    console.log(cars[i]);
+}
+
+// comparei direto o indice com array cars => i(0) < cars.length(3)
+for(let i = 0; i< cars.length; i++){
+    console.log(cars[i]);
+}
+
+// O i é uma variável de controle do loop — ela vai “andando” pelos índices do array,
+//  e o cars[i] usa esse valor pra acessar cada item da lista.
